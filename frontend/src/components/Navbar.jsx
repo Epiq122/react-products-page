1e32853b7d787e37a5be60b59debcd9c29026c55
Robert Gleason <l2ob122@gmail.com>
2024-09-28 22:27:40 -0700

    added logic to add items from a form and put into the DB, also added toast functionality to show success inside the create page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { PlusSquareIcon } from '@chakra-ui/icons';
-import { useColorMode} from '@chakra-ui/color-mode';
+import { useColorMode } from '@chakra-ui/color-mode';
 import { IoMoon } from 'react-icons/io5';
 import { LuSun } from 'react-icons/lu';
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={'1140px'} px={4}>
       <Flex
